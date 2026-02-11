52c198d688d61053c93c7bec68bfe30a9ee3ddca
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-02-10 20:02:34 -0500

    Update to moonc v0.8.1+bd827dc85
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 DIRS="hello-world kv-store llm now outbound-http redis variables"
 for i in ${DIRS}; do
     pushd $i > /dev/null
-    moon update && moon install && rm -rf target
+    moon update && rm -rf target
     moon fmt
     # As of moonc v0.6.31+b5b06ff93, `--target native` no longer works.
     # moon test --target all
